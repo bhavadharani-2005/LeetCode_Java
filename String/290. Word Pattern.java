a9d68c4a876ab0aa2c6968e5fdc9bca5901a481a
Bhava dharani <bhavadharani2005tn47@gmail.com>
2026-07-31 23:16:14 +0530

    Implement word pattern matching solution

---
+++ String/290. Word Pattern.java
@@ -0,0 +1,23 @@
+import java.util.Collection;
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character, String> hm = new HashMap<>();
+        String arr[] = s.trim().split(" ");
+        if (pattern.length() != arr.length) return false;
+        for (int i = 0; i < pattern.length(); i++) {
+            char ch = pattern.charAt(i);
+            if (hm.containsKey(ch)) {
+                if (!hm.get(ch).equals(arr[i])) {
+                    return false;
+                }
+            }
+            else {
+                if (hm.containsValue(arr[i])) 
+                    return false;
+
+                hm.put(ch, arr[i]);
+            }
+        }
+        return true;
+    }
+}
