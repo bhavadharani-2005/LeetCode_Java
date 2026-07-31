154a58afad4625a2ef2b290de8c4072b5980ddcf
Bhava dharani <bhavadharani2005tn47@gmail.com>
2026-07-31 21:52:04 +0530

    Add Sudoku validation method
    Implement a method to validate a Sudoku board.

---
+++ Arrays/36. Valid Sudoku.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char num = board[i][j];
+                if (num != '.') {
+                    if (!seen.add(num + " in row " + i) ||
+                        !seen.add(num + " in col " + j) ||
+                        !seen.add(num + " in box " + (i / 3) + "-" + (j / 3)))
+                        return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        char[][] board = {
+            {'5','3','.','.','7','.','.','.','.'},
+            {'6','.','.','1','9','5','.','.','.'},
+            {'.','9','8','.','.','.','.','6','.'},
+            {'8','.','.','.','6','.','.','.','3'},
+            {'4','.','.','8','.','3','.','.','1'},
+            {'7','.','.','.','2','.','.','.','6'},
+            {'.','6','.','.','.','.','2','8','.'},
+            {'.','.','.','4','1','9','.','.','5'},
+            {'.','.','.','.','8','.','.','7','9'}
+        };
+        System.out.println(s.isValidSudoku(board));
+    }
+}
