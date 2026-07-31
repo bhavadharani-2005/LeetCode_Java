3b37087244132ccdb412592ef704daa652198644
Bhava dharani <bhavadharani2005tn47@gmail.com>
2026-07-31 23:32:11 +0530

    Implement solution for maximum words typing problem

---
+++ String/1935. Maximum Number of Words You Can Type.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        boolean[] broken = new boolean[26];
+        for (char ch : brokenLetters.toCharArray()) {
+            broken[ch - 'a'] = true; 
+        }
+
+        String[] words = text.split(" ");
+        int count = 0;
+
+        for (String word : words) {
+            boolean canType = true;
+            for (char ch : word.toCharArray()) {
+                if (broken[ch - 'a']) { 
+                    canType = false;
+                    break;
+                }
+            }
+            if (canType) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
