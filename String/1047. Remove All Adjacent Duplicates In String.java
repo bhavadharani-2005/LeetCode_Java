af4ced8f651c704920486887a322fc527ee81e6f
Bhava dharani <bhavadharani2005tn47@gmail.com>
2026-07-31 23:27:03 +0530

    Create 1047. Remove All Adjacent Duplicates In String.java

---
+++ String/1047. Remove All Adjacent Duplicates In String.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        StringBuilder stack = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            int len = stack.length();
+
+            if (len > 0 && stack.charAt(len - 1) == c) {
+                stack.deleteCharAt(len - 1); // pop
+            } else {
+                stack.append(c); // push
+            }
+        }
+
+        return stack.toString();
+    }
+}
